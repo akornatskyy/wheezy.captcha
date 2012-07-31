67e239e46f3b61aa084245aa7c8df42ce99a52e3
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2012-07-31 14:21:09 +0300

    Minor cleanup in demo sample.

--- demos/sample.py
+++ demos/sample.py
@@ -1,20 +1,20 @@
 """
 """
 
-import random
-
 from wheezy.captcha.image import captcha
+
 from wheezy.captcha.image import background
-from wheezy.captcha.image import text
 from wheezy.captcha.image import noise
 from wheezy.captcha.image import smooth
+from wheezy.captcha.image import text
 
-from wheezy.captcha.image import warp
-from wheezy.captcha.image import rotate
 from wheezy.captcha.image import offset
+from wheezy.captcha.image import rotate
+from wheezy.captcha.image import warp
 
 
 if __name__ == '__main__':
+    import random
     import string
     captcha_image = captcha(drawings=[
         background(),
